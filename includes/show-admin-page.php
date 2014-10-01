82b160917adc368af445dc58ed8380b28636cdb9
rtpHarry <rtpharry@hotmail.com>
2014-10-01 03:38:26 +0100

    Added license.txt + header snippets

--- includes/show-admin-page.php
+++ includes/show-admin-page.php
@@ -1,5 +1,27 @@
 <?php
-$BPSC_DEBUG = true;
+/*
+    BulkPageStubCreator-WordPress v1.0
+    Copyright (C) 2014 Matthew Harris aka rtpHarry
+
+    Bulk create page stubs by simply providing a plain text list of titles and slugs.
+
+    http://articles.runtings.co.uk/p/bulk-page-stub-creator-wordpress.html
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+$BPSC_DEBUG = false; // if set to true then the <textarea> is prepopulated with some sample data
 
 function bpsc_extract_info() {
 	$input = $_POST["bpsc_pagestocreate"]; // grab textarea contents 
