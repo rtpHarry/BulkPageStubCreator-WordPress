1d7cdee8bb99fdb794b3f36cd64b83f0985b1b5f
Matthew Harris <rtpharry@hotmail.com>
2025-04-24 23:57:11 +0100

    refactor: convert to class files

--- includes/process-data.php
+++ includes/process-data.php
@@ -1,125 +1,127 @@
 <?php
 
-/*
-    BulkPageStubCreator-WordPress v1.1
-    Copyright (C) 2014 Matthew Harris aka rtpHarry
-
-    Bulk create page stubs by simply providing a plain text list of titles and slugs.
-
-    http://articles.runtings.co.uk/p/bulk-page-stub-creator-wordpress.html
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 namespace RunthingsBulkPageStubCreator;
 
 if (!defined('WPINC')) {
     die;
 }
 
-function bpsc_check_slug_for_error_level($slug_requested, $slug_returned) {
-	$slug_requested = sanitize_text_field($slug_requested);
-	$slug_returned = sanitize_text_field($slug_returned);
-	
-	$slug_sanitized = sanitize_title_with_dashes($slug_requested);
-	
-	if((strcmp($slug_requested, $slug_sanitized) != 0) 
-		|| (strcmp($slug_requested, $slug_returned) != 0)) {
-			// requested slug was not sanitized or was in use
-			return "url-alternative-was-used";
-	}
-	
-	// request slug was used for post
-	return "none";
-}
+class Processor {    
+    /**
+     * Check if the requested slug was successfully used or modified
+     *
+     * @param string $slug_requested The slug that was requested
+     * @param string $slug_returned The slug that was actually used
+     * @return string Error level or 'none' if no error
+     */
+    public function check_slug_for_error_level($slug_requested, $slug_returned) {
+        $slug_requested = sanitize_text_field($slug_requested);
+        $slug_returned = sanitize_text_field($slug_returned);
+        
+        $slug_sanitized = sanitize_title_with_dashes($slug_requested);
+        
+        if((strcmp($slug_requested, $slug_sanitized) != 0) 
+            || (strcmp($slug_requested, $slug_returned) != 0)) {
+                // requested slug was not sanitized or was in use
+                return "url-alternative-was-used";
+        }
+        
+        // request slug was used for post
+        return "none";
+    }
 
-function bpsc_create_result_array_element($error_level, $post_title, $post_name, $post_id) {
-	return array(
-		'error_level' => sanitize_text_field($error_level),
-		'post_title' => sanitize_text_field($post_title),
-		'post_name' => sanitize_text_field($post_name),
-		'post_id' => intval($post_id)
-	);
-}
-
-function bpsc_bulk_create_pages($extracted_info) {
-    // Validate user capabilities before performing operations
-    if (!current_user_can('publish_pages')) {
+    /**
+     * Create a standardized result array element
+     *
+     * @param string $error_level Error level or 'none'
+     * @param string $post_title Post title
+     * @param string $post_name Post slug
+     * @param int $post_id Post ID
+     * @return array Result array
+     */
+    public function create_result_array_element($error_level, $post_title, $post_name, $post_id) {
         return array(
-            bpsc_create_result_array_element(
-                'error-insufficient-permissions',
-                esc_html__('Permission Denied'),
-                '',
-                0
-            )
+            'error_level' => sanitize_text_field($error_level),
+            'post_title' => sanitize_text_field($post_title),
+            'post_name' => sanitize_text_field($post_name),
+            'post_id' => intval($post_id)
         );
     }
 
-    $results = array();
+    /**
+     * Bulk create pages from extracted info
+     *
+     * @param array $extracted_info Array of page titles and slugs
+     * @return array Results of page creation
+     */
+    public function bulk_create_pages($extracted_info) {
+        // Validate user capabilities before performing operations
+        if (!current_user_can('publish_pages')) {
+            return array(
+                $this->create_result_array_element(
+                    'error-insufficient-permissions',
+                    esc_html__('Permission Denied'),
+                    '',
+                    0
+                )
+            );
+        }
 
-    if (!is_array($extracted_info) || empty($extracted_info)) {
+        $results = array();
+
+        if (!is_array($extracted_info) || empty($extracted_info)) {
+            return $results;
+        }
+
+        for($i = 0, $size = count($extracted_info); $i < $size; $i = $i + 2) {
+            // Ensure array indexes exist
+            if (!isset($extracted_info[$i]) || !isset($extracted_info[$i + 1])) {
+                continue;
+            }
+            
+            $post_title = sanitize_text_field($extracted_info[$i]);
+            $post_name = sanitize_title($extracted_info[$i + 1]);
+            
+            $post_to_add = array(
+                'post_title' => $post_title,
+                'post_name' => $post_name,
+                'post_status' => 'publish',
+                'post_type' => 'page'
+            );
+            
+            $last_post_id = wp_insert_post($post_to_add);
+                    
+            if($last_post_id == 0) {
+                // log error
+                array_push($results, $this->create_result_array_element(
+                    "wp-insert-post-error", 
+                    $post_to_add['post_title'], 
+                    $post_to_add['post_name'], 
+                    $last_post_id));            
+            }
+            else {
+                // log post details
+                $last_post = get_post($last_post_id);
+                
+                if (!$last_post) {
+                    // Handle case where get_post fails
+                    array_push($results, $this->create_result_array_element(
+                        "get-post-error",
+                        esc_html($post_to_add['post_title']),
+                        esc_html($post_to_add['post_name']),
+                        $last_post_id
+                    ));
+                    continue;
+                }
+                            
+                array_push($results, $this->create_result_array_element(
+                    $this->check_slug_for_error_level($post_to_add['post_name'], $last_post->post_name),
+                    $last_post->post_title, 
+                    $last_post->post_name, 
+                    $last_post_id));
+            }        
+        }
+        
         return $results;
     }
-
-	for($i = 0, $size = count($extracted_info); $i < $size; $i = $i + 2) {
-		// Ensure array indexes exist
-		if (!isset($extracted_info[$i]) || !isset($extracted_info[$i + 1])) {
-			continue;
-		}
-		
-		$post_title = sanitize_text_field($extracted_info[$i]);
-		$post_name = sanitize_title($extracted_info[$i + 1]);
-		
-		$post_to_add = array(
-			'post_title' => $post_title,
-			'post_name' => $post_name,
-			'post_status' => 'publish',
-			'post_type' => 'page'
-		);
-		
-		$last_post_id = wp_insert_post($post_to_add);
-				
-		if($last_post_id == 0) {
-			// log error
-			array_push($results, bpsc_create_result_array_element(
-				"wp-insert-post-error", 
-				$post_to_add['post_title'], 
-				$post_to_add['post_name'], 
-				$last_post_id));			
-		}
-		else {
-			// log post details
-			$last_post = get_post($last_post_id);
-			
-			if (!$last_post) {
-				// Handle case where get_post fails
-				array_push($results, bpsc_create_result_array_element(
-					"get-post-error",
-					esc_html($post_to_add['post_title']),
-					esc_html($post_to_add['post_name']),
-					$last_post_id
-				));
-				continue;
-			}
-						
-			array_push($results, bpsc_create_result_array_element(
-				bpsc_check_slug_for_error_level($post_to_add['post_name'], $last_post->post_name),
-				$last_post->post_title, 
-				$last_post->post_name, 
-				$last_post_id));
-		}		
-	}
-	
-	return $results;
 }
--- includes/show-admin-page.php
+++ includes/show-admin-page.php
@@ -1,25 +1,4 @@
 <?php
-/*
-    BulkPageStubCreator-WordPress v1.1
-    Copyright (C) 2014 Matthew Harris aka rtpHarry
-
-    Bulk create page stubs by simply providing a plain text list of titles and slugs.
-
-    http://articles.runtings.co.uk/p/bulk-page-stub-creator-wordpress.html
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 namespace RunthingsBulkPageStubCreator;
 
@@ -27,115 +6,160 @@ if (!defined('WPINC')) {
     die;
 }
 
+// Define debug mode constant if not already defined
 if (!defined('BPSC_DEBUG')) {
-    define('BPSC_DEBUG', false); // if set to true then the <textarea> is prepopulated with some sample data
+    // In production environments, always set to false
+    // In development, allow enabling via wp-config.php
+    if (defined('WP_DEBUG') && WP_DEBUG === true) {
+        define('BPSC_DEBUG', false); // Still false by default, but can be overridden
+    } else {
+        define('BPSC_DEBUG', false); // Always false in production
+    }
 }
 
-function bpsc_extract_info() {
-    // Check user capabilities before processing any data
-    if (!current_user_can('publish_pages')) {
-        return array();
+class Admin {
+    /**
+     * @var Processor Data processing instance
+     */
+    private $processor;
+    
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        $this->processor = new Processor();
+        add_action('admin_menu', array($this, 'add_admin_page_link'));
     }
     
-    // Sanitize and validate the textarea content
-    $input = '';
-    if (isset($_POST["bpsc_pagestocreate"])) {
-        // Use sanitize_textarea_field to properly handle the multiline input
-        $input = sanitize_textarea_field($_POST["bpsc_pagestocreate"]);
+    /**
+     * Extract info from the textarea input
+     *
+     * @return array Extracted info
+     */
+    private function extract_info() {
+        // Check user capabilities before processing any data
+        if (!current_user_can('publish_pages')) {
+            return array();
+        }
+        
+        // Sanitize and validate the textarea content
+        $input = '';
+        if (isset($_POST["bpsc_pagestocreate"])) {
+            // Use sanitize_textarea_field to properly handle the multiline input
+            $input = sanitize_textarea_field($_POST["bpsc_pagestocreate"]);
+        }
+        
+        // Just handle different line endings properly
+        $input = str_replace(array("\r\n", "\r"), "\n", $input);
+        $extracted_info = explode("\n", $input); // split into array
+        
+        return $extracted_info;
     }
-    
-    // Just handle different line endings properly
-    $input = str_replace(array("\r\n", "\r"), "\n", $input);
-    $extracted_info = explode("\n", $input); // split into array
-    
-    return $extracted_info;
-}
 
-function bpsc_process_admin_page($extracted_info) {
-    // Validate user capabilities
-    if (!current_user_can('publish_pages')) {
-        return array();
+    /**
+     * Process extracted info and create pages
+     *
+     * @param array $extracted_info Info extracted from textarea
+     * @return array Results of page creation
+     */
+    private function process_admin_page($extracted_info) {
+        // Validate user capabilities
+        if (!current_user_can('publish_pages')) {
+            return array();
+        }
+        
+        // Add basic validation before processing
+        if (empty($extracted_info)) {
+            return array();
+        }
+        
+        $results = $this->processor->bulk_create_pages($extracted_info);
+        return $results;
     }
-    
-    // Add basic validation before processing
-    if (empty($extracted_info)) {
-        return array();
-    }
-    
-    $results = bpsc_bulk_create_pages($extracted_info);
-    return $results;
-}
 
-function bpsc_display_admin_results_page($results) {
-    // Verify permissions before displaying admin content
-    if (!current_user_can('publish_pages')) {
-        wp_die(__('You do not have sufficient permissions to access this page.'));
-    }
-    
-    // show output
-    ob_start(); ?>
-    <div class="wrap">
-        <h2><?php esc_html_e('Bulk Page Stub Creator'); ?></h2>
-        <h3><?php esc_html_e('Bulk Page Creation Results'); ?></h3>
-        <p><?php esc_html_e('Page results listed below, click the links to edit the pages'); ?></p>
-        <p>
-        <?php
-        if (empty($results)) {
-            esc_html_e('No pages were created. Please check your input and try again.');
-        } else {
-            foreach($results as $result) {
-                $css_class = isset($result['error_level']) ? esc_attr($result['error_level']) : 'none';
-                $post_id = isset($result['post_id']) ? intval($result['post_id']) : 0;
-                $post_title = isset($result['post_title']) ? esc_html($result['post_title']) : '';
-                
-                if ($post_id > 0) {
-                    echo '<a target="_blank" class="' . $css_class . '" href="' . 
-                         esc_url(admin_url('post.php?action=edit&post=' . $post_id)) . 
-                         '">' . $post_title . '</a>';
+    /**
+     * Display the results page after page creation
+     *
+     * @param array $results Results of page creation
+     */
+    private function display_admin_results_page($results) {
+        // Verify permissions before displaying admin content
+        if (!current_user_can('publish_pages')) {
+            wp_die(__('You do not have sufficient permissions to access this page.'));
+        }
+        
+        // show output
+        ob_start(); ?>
+        <div class="wrap">
+            <h2><?php esc_html_e('Bulk Page Stub Creator'); ?></h2>
+            <h3><?php esc_html_e('Bulk Page Creation Results'); ?></h3>
+            <p><?php esc_html_e('Page results listed below, click the links to edit the pages'); ?></p>
+            <p>
+            <?php
+            if (empty($results)) {
+                esc_html_e('No pages were created. Please check your input and try again.');
+            } else {
+                foreach($results as $result) {
+                    $css_class = isset($result['error_level']) ? esc_attr($result['error_level']) : 'none';
+                    $post_id = isset($result['post_id']) ? intval($result['post_id']) : 0;
+                    $post_title = isset($result['post_title']) ? esc_html($result['post_title']) : '';
                     
-                    if (strcmp($css_class, "none") != 0 && isset($result['post_name'])) {
-                        printf(
-                            ' (<strong style="color: #ff0000;">%s</strong> %s /%s)',
-                            esc_html__('ERROR:'),
-                            esc_html__('requested slug invalid or in use, page slug is:'),
-                            esc_html($result['post_name'])
-                        );
+                    if ($post_id > 0) {
+                        echo '<a target="_blank" class="' . $css_class . '" href="' . 
+                             esc_url(admin_url('post.php?action=edit&post=' . $post_id)) . 
+                             '">' . $post_title . '</a>';
+                        
+                        if (strcmp($css_class, "none") != 0 && isset($result['post_name'])) {
+                            printf(
+                                ' (<strong style="color: #ff0000;">%s</strong> %s /%s)',
+                                esc_html__('ERROR:'),
+                                esc_html__('requested slug invalid or in use, page slug is:'),
+                                esc_html($result['post_name'])
+                            );
+                        }
+                        
+                        echo "<br>";
                     }
-                    
-                    echo "<br>";
                 }
             }
-        }
-        ?>
-        </p>
-        
-        <form method="post" action="">
-        <?php wp_nonce_field('bpsc_return_action', 'bpsc_return_nonce'); ?>
-        <p>
-            <input class="button-primary" type="submit" name="save" value="<?php esc_attr_e('Return to main page'); ?>" id="submitbutton" />
-        </p>
-        </form>        
-    </div>
-    <?php
-    echo ob_get_clean();   
-}
-
-function bpsc_display_admin_page($is_uneven_inputs_error = NULL, $input = NULL) {
-    // Verify permissions before displaying admin content
-    if (!current_user_can('publish_pages')) {
-        wp_die(__('You do not have sufficient permissions to access this page.'));
+            ?>
+            </p>
+            
+            <form method="post" action="">
+            <?php wp_nonce_field('bpsc_return_action', 'bpsc_return_nonce'); ?>
+            <p>
+                <input class="button-primary" type="submit" name="save" value="<?php esc_attr_e('Return to main page'); ?>" id="submitbutton" />
+            </p>
+            </form>        
+        </div>
+        <?php
+        echo ob_get_clean();   
     }
-    
-    ob_start(); ?>
-    <div class="wrap">
-        <h2><div id="icon-edit-pages" class="icon32"></div> <?php esc_html_e('Bulk Page Stub Creator'); ?></h2>
-        <?php if(BPSC_DEBUG) { ?>
-            <div style="background: #E7373A; color: #fff; padding: 10px; font-weight: bold;"><?php esc_html_e('WARNING - BPSC_DEBUG = true; - disable before deploying'); ?></div>
-        <?php } ?>
+
+    /**
+     * Display the main admin page
+     *
+     * @param bool $is_uneven_inputs_error Whether an error occurred due to uneven inputs
+     * @param string $input The original input to redisplay
+     */
+    private function display_admin_page($is_uneven_inputs_error = NULL, $input = NULL) {
+        // Verify permissions before displaying admin content
+        if (!current_user_can('publish_pages')) {
+            wp_die(__('You do not have sufficient permissions to access this page.'));
+        }
         
-        <p><?php esc_html_e("Enter the pages into the text area below, one line for the page title, one line for the url, then repeat for as many page stubs that you want to create."); ?></p>
-        <h4><?php esc_html_e('Example'); ?></h4>
-        <pre><?php echo esc_html("Some Page
+        ob_start(); ?>
+        <div class="wrap">
+            <h2><div id="icon-edit-pages" class="icon32"></div> <?php esc_html_e('Bulk Page Stub Creator'); ?></h2>
+            <?php if(BPSC_DEBUG) { ?>
+                <div class="notice notice-error">
+                    <p><strong><?php esc_html_e('WARNING - Debug mode is enabled. Disable before deploying to production.'); ?></strong></p>
+                </div>
+            <?php } ?>
+            
+            <p><?php esc_html_e("Enter the pages into the text area below, one line for the page title, one line for the url, then repeat for as many page stubs that you want to create."); ?></p>
+            <h4><?php esc_html_e('Example'); ?></h4>
+            <pre><?php echo esc_html("Some Page
 optimised-url-for-some-page
 Another Page Title Here
 custom-url-for-another-page
@@ -143,19 +167,19 @@ Site Map
 site-map
 Contact Us
 contact-this-company"); ?></pre>
-        <form method="post" action="">
-        <?php wp_nonce_field('bpsc_create_pages_action', 'bpsc_create_pages_nonce'); ?>
-        <h4><?php esc_html_e("Bulk Create Pages"); ?></h4>
-        <p>
-            <label class="description" for="bpsc_pagestocreate"><?php esc_html_e('Enter the site map data for the pages you want to create'); ?>:</label><br>
-            <?php if($is_uneven_inputs_error) { ?>
-            <strong style='color: #ff0000;'><?php esc_html_e('ERROR:'); ?></strong> <?php esc_html_e('You have not supplied an even number of inputs.'); ?></p>
-            <?php } ?>
-            <textarea id="bpsc_pagestocreate" name="bpsc_pagestocreate" rows="20" class="large-text code"><?php 
-                if($is_uneven_inputs_error == true) { 
-                    echo esc_textarea($input); 
-                } elseif (BPSC_DEBUG == true) { 
-                    echo esc_textarea("Some Page
+            <form method="post" action="">
+            <?php wp_nonce_field('bpsc_create_pages_action', 'bpsc_create_pages_nonce'); ?>
+            <h4><?php esc_html_e("Bulk Create Pages"); ?></h4>
+            <p>
+                <label class="description" for="bpsc_pagestocreate"><?php esc_html_e('Enter the site map data for the pages you want to create'); ?>:</label><br>
+                <?php if($is_uneven_inputs_error) { ?>
+                <strong style='color: #ff0000;'><?php esc_html_e('ERROR:'); ?></strong> <?php esc_html_e('You have not supplied an even number of inputs.'); ?></p>
+                <?php } ?>
+                <textarea id="bpsc_pagestocreate" name="bpsc_pagestocreate" rows="20" class="large-text code"><?php 
+                    if($is_uneven_inputs_error == true) { 
+                        echo esc_textarea($input); 
+                    } elseif (BPSC_DEBUG) { 
+                        echo esc_textarea("Some Page
 optimised-url-for-some-page
 Another Page Title Here
 custom-url-for-another-page
@@ -163,50 +187,59 @@ Site Map
 site-map
 Contact Us
 contact-this-company"); 
-                } 
-            ?></textarea>
-        </p>
-        <p>
-            <input class="button-primary" type="submit" name="save" value='<?php esc_attr_e("Create page stubs"); ?>' id="submitbutton" />
-        </p>
-        </form>
-    </div>
-    <?php
-    echo ob_get_clean();    	
-}
-
-function bspc_admin_page() {
-    // Main capability check - verify user can access this admin page
-    if (!current_user_can('publish_pages')) {
-        wp_die(__('You do not have sufficient permissions to access this page.'));
+                    } 
+                ?></textarea>
+            </p>
+            <p>
+                <input class="button-primary" type="submit" name="save" value='<?php esc_attr_e("Create page stubs"); ?>' id="submitbutton" />
+            </p>
+            </form>
+        </div>
+        <?php
+        echo ob_get_clean();    	
     }
-    
-    // Verify nonce when processing form submissions
-    if (isset($_POST["bpsc_pagestocreate"])) {
-        // Verify the nonce for page creation
-        if (!isset($_POST['bpsc_create_pages_nonce']) || 
-            !wp_verify_nonce($_POST['bpsc_create_pages_nonce'], 'bpsc_create_pages_action')) {
-            wp_die(__('Security check failed. Please try again.'));
+
+    /**
+     * Main admin page handler
+     */
+    public function admin_page() {
+        // Main capability check - verify user can access this admin page
+        if (!current_user_can('publish_pages')) {
+            wp_die(__('You do not have sufficient permissions to access this page.'));
         }
         
-        $extracted_info = bpsc_extract_info();
-        
-        // check even number of inputs
-        if((count($extracted_info) % 2) == 1) {
-            bpsc_display_admin_page(true, $_POST["bpsc_pagestocreate"]);
-        } else {        
-            $results = bpsc_process_admin_page($extracted_info);
-            bpsc_display_admin_results_page($results);
+        // Verify nonce when processing form submissions
+        if (isset($_POST["bpsc_pagestocreate"])) {
+            // Verify the nonce for page creation
+            if (!isset($_POST['bpsc_create_pages_nonce']) || 
+                !wp_verify_nonce($_POST['bpsc_create_pages_nonce'], 'bpsc_create_pages_action')) {
+                wp_die(__('Security check failed. Please try again.'));
+            }
+            
+            $extracted_info = $this->extract_info();
+            
+            // check even number of inputs
+            if((count($extracted_info) % 2) == 1) {
+                $this->display_admin_page(true, $_POST["bpsc_pagestocreate"]);
+            } else {        
+                $results = $this->process_admin_page($extracted_info);
+                $this->display_admin_results_page($results);
+            }
+        }
+        else {
+            $this->display_admin_page();
         }
     }
-    else {
-        bpsc_display_admin_page();
+
+    /**
+     * Add the admin page link to the WordPress menu
+     */
+    public function add_admin_page_link() {
+        // The capability check is already handled by the 3rd parameter in add_management_page()
+        add_management_page("Bulk Page Stub Creator", "Bulk Page Stub Creator", "publish_pages", 
+            "bulk-page-stub-creator", array($this, 'admin_page'));
     }
 }
 
-function bpsc_add_admin_page_link() {
-    // The capability check is already handled by the 3rd parameter in add_management_page()
-    add_management_page("Bulk Page Stub Creator", "Bulk Page Stub Creator", "publish_pages", "bulk-page-stub-creator", "bspc_admin_page");
-}
- 
-add_action('admin_menu', 'bpsc_add_admin_page_link');
+// Initialize the Admin class
+new Admin();
